9cc243c0ef3a3c20678e94dcdb3fc0f4e279aae0
Alexander Lobakin <aleksander.lobakin@intel.com>
2023-12-11 12:27:39 +0100

    libie: add a couple of XDP helpers
    Signed-off-by: Alexander Lobakin <aleksander.lobakin@intel.com>

---
+++ drivers/net/ethernet/intel/libie/tx.c
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/* Copyright(c) 2023 Intel Corporation. */
+
+#include <linux/net/intel/libie/xdp.h>
+
+void libie_tx_complete_any(struct libie_tx_buffer *buf, struct device *dev,
+			   struct xdp_frame_bulk *bq,
+			   struct libie_sq_onstack_stats *ss)
+{
+	if (buf->type > LIBIE_TX_BUF_SKB)
+		libie_xdp_complete_tx_buf(buf, dev, false, bq, ss);
+	else
+		libie_tx_complete_buf(buf, dev, false, ss);
+}
+EXPORT_SYMBOL_NS_GPL(libie_tx_complete_any, LIBIE);
