31af32f8291c1de2116e652349777c2aecf053b5
Alexander Lobakin <aleksander.lobakin@intel.com>
2023-12-12 17:01:52 +0100

    idpf: add support for XDP on Rx
    Use libeth XDP infra to support running XDP program on Rx polling.
    This includes all of the possible verdicts/actions.
    XDP Tx queues are cleaned only in "lazy" mode when there are less than
    1/4 free descriptors left on the ring. libeth helper macros to define
    driver-specific XDP functions make sure the compiler could uninline
    them when needed.
    Use __LIBETH_WORD_ACCESS to parse descriptors more efficiently when
    applicable. It really gives some good boosts and code size reduction
    on x86_64.
    
    Co-developed-by: Michal Kubiak <michal.kubiak@intel.com>
    Signed-off-by: Michal Kubiak <michal.kubiak@intel.com>
    Signed-off-by: Alexander Lobakin <aleksander.lobakin@intel.com>

--- drivers/net/ethernet/intel/idpf/idpf_txrx.h
+++ drivers/net/ethernet/intel/idpf/idpf_txrx.h
@@ -702,8 +702,8 @@ struct idpf_tx_queue {
 	__cacheline_group_end_aligned(read_mostly);
 
 	__cacheline_group_begin_aligned(read_write);
-	u16 next_to_use;
-	u16 next_to_clean;
+	u32 next_to_use;
+	u32 next_to_clean;
 
 	union {
 		struct {
@@ -746,7 +746,7 @@ struct idpf_tx_queue {
  * hardcode any assumptions.
  */
 libeth_cacheline_set_assert(struct idpf_tx_queue, 64,
-			    4 + offsetofend(struct idpf_tx_queue, q_stats) -
+			    8 + offsetofend(struct idpf_tx_queue, q_stats) -
 			    offsetofend(struct idpf_tx_queue, next_to_clean),
 			    24);
 
