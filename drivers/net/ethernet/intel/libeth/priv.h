b173e919a9fb5bd89820e95b611dd1543875a2f5
Alexander Lobakin <aleksander.lobakin@intel.com>
2024-11-28 16:55:51 +0100

    libeth_xdp: add XSk helpers
    Suggested-by: Maciej Fijalkowski <maciej.fijalkowski@intel.com> # lots of stuff
    Signed-off-by: Alexander Lobakin <aleksander.lobakin@intel.com>

--- drivers/net/ethernet/intel/libeth/priv.h
+++ drivers/net/ethernet/intel/libeth/priv.h
@@ -8,12 +8,23 @@
 
 /* XDP */
 
+enum xdp_action;
+struct libeth_xdp_buff;
+struct libeth_xdp_tx_frame;
 struct skb_shared_info;
 struct xdp_frame_bulk;
+
+extern const struct xsk_tx_metadata_ops libeth_xsktmo_slow;
+
+void libeth_xsk_tx_return_bulk(const struct libeth_xdp_tx_frame *bq,
+			       u32 count);
+u32 libeth_xsk_prog_exception(struct libeth_xdp_buff *xdp, enum xdp_action act,
+			      int ret);
 
 struct libeth_xdp_ops {
 	void	(*bulk)(const struct skb_shared_info *sinfo,
 			struct xdp_frame_bulk *bq, bool frags);
+	void	(*xsk)(struct libeth_xdp_buff *xdp);
 };
 
 void libeth_attach_xdp(const struct libeth_xdp_ops *ops);
