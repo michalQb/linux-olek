adb33686a14912439f1486144cd709023200c6a0
Larysa Zaremba <larysa.zaremba@intel.com>
2025-05-16 16:58:07 +0200

    idpf: make mbx_task queueing and cancelling more consistent
    As a consequence of refactoring idpf code to use libeth APIs,
    idpf_vc_xn_shutdown was merged with and replaced by idpf_deinit_dflt_mbx.
    This does not affect the Tx path, as it checked for a presence of an xn
    manager anyway. Rx processing is handled by the mbx_task that is not always
    cancelled before calling the new consolidated mailbox deinit function.
    Moreover, in the reset path idpf_intr_rel() reschedules it after the deinit
    is done. This leads to mbx_task referencing the freed mailbox and causing
    KASAN warnings.
    
    To remedy this, in the init path, do the first queueing of mbx_task in
    idpf_init_dflt_mbx(), in deinit and reset, always cancel the task in
    idpf_deinit_dflt_mbx() and in every flow first call idpf_mb_intr_rel_irq().
    
    Reviewed-by: Michal Kubiak <michal.kubiak@intel.com>
    Signed-off-by: Larysa Zaremba <larysa.zaremba@intel.com>

--- drivers/net/ethernet/intel/idpf/idpf_virtchnl.c
+++ drivers/net/ethernet/intel/idpf/idpf_virtchnl.c
@@ -2357,6 +2357,8 @@ int idpf_init_dflt_mbx(struct idpf_adapter *adapter)
 	adapter->xn_init_params.xnm = params.xnm;
 	adapter->state = __IDPF_VER_CHECK;
 
+	queue_delayed_work(adapter->mbx_wq, &adapter->mbx_task, 0);
+
 	return 0;
 }
 
@@ -2366,6 +2368,9 @@ int idpf_init_dflt_mbx(struct idpf_adapter *adapter)
  */
 void idpf_deinit_dflt_mbx(struct idpf_adapter *adapter)
 {
+	idpf_mb_intr_rel_irq(adapter);
+	cancel_delayed_work_sync(&adapter->mbx_task);
+
 	if (adapter->arq && adapter->asq) {
 		idpf_mb_clean(adapter, adapter->asq);
 		libie_ctlq_xn_deinit(adapter->xn_init_params.xnm,
@@ -2606,7 +2611,6 @@ void idpf_vc_core_deinit(struct idpf_adapter *adapter)
 		idpf_deinit_dflt_mbx(adapter);
 
 	cancel_delayed_work_sync(&adapter->serv_task);
-	cancel_delayed_work_sync(&adapter->mbx_task);
 
 	idpf_vport_params_buf_rel(adapter);
 
